1b86b5a802bd56bd91a806b59fd332c36aaaa7b8
DeliaNav <mennavd@gmail.com>
2026-04-21 11:45:52 +0200

    Base del view/welcome

---
+++ Codigo/views/welcome.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TFG</title>
+</head>
+<body>
+    <div class="hero">
+        <div class="hero-bg">
+            <div class="orb orb1"></div>
+            <div class="orb orb2"></div>
+            <div class="orb orb3"></div>
+        </div>
+
+        <nav class="top-nav">
+            <div class="nav-logo">
+                <span class="logo-icon"></span><!--Logo ver de donde se saca-->
+                <span class="logo-text"></span><!--texto TCG Market/Shinny Hunt ...-->
+            </div>
+            <div class="nav-actions">
+                <a href="/TFG/Codigo/login" class="btn btn-ghost">Iniciar Sesión</a>
+                <a href="/TFG/Codigo/registro" class="btn btn-primary">Registrarse</a>
+            </div>
+        </nav>
+
+        <div class="hero-content">
+            <div class="hro-badge">Shinny Hunt</div>
+            <h1 class="hero-title">Tu mercado de cartas pokemon</h1>
+            <p class="hero-subtitle">descripción de que web y eso, tipo: gestiona tu coleccion o algo asi</p>
+        </div>
+
+        <div class="features">
+            <!--Botones que redireccionan a colecciones, lista de deseos o algo asi-->
+        </div>
+
+    </div>
+</body>
+</html>
