d77c8fc7bee2c676e914aa4be46317a342ca4423
DeliaNav <mennavd@gmail.com>
2026-04-27 12:21:45 +0200

    Collection model

--- Codigo/models/Collection.php
+++ Codigo/models/Collection.php
@@ -17,22 +17,35 @@ class Collection {
         return $stmt->fetchAll();
     }
 
-    public function add(){
+    public function add(int $userId, string $cardId, string $cardName, string $imageUrl): bool{
         //anadir cartas a wishlist
+        $stmt = $this->pdo->prepare("INSERT INTO collections (user_id, card_id, card_name, image_url, quantity)
+            VALUES (?, ?, ?, ?, 1)
+            ON DUPLICATE KEY UPDATE quantity = quantity + 1");
+
+        return $stmt->execute([$userId, $cardId, $cardId, $cardName, $imageUrl]);
     }
 
-    public function remove(){
-        //adivina que hace
+    public function remove(int $userId, string $cardId){
         //elimina la carta
+        $stmt = $this->pdo->prepare("DELETE FROM collections WHERE user_id = ? AND card_id = ?");
+        return $stmt->execute([$userId, $cardId]);
     }
 
-    public function hasCard(){
+    public function hasCard(int $userId, string $cardId): bool {
         //comprobacion de si tiene carta (mejor en metodo separado)
+        $stmt = $this->pdo->prepare("SELECT id FROM collections WHERE user_id = ? AND card_id = ?");
+        $stmt->execute([$userId, $cardId]);
+
+        return (bool) $stmt->fetch();
     }
 
-    public function count(){
+    public function count(int $userId): int {
         //toal de cartas
-    
+        $stmt = $this->pdo->prepare("SELECT COALESCE(SUM(quantity), 0) FROM collections WHERE user_id = ?");
+        //COALESCE devuelve primer valor no nulo encontrado
+        $stmt->execute([$userId]);
+        return (int) $stmt-fetchColumn();
     }
 
 }
