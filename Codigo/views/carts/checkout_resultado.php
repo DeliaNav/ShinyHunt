c4dac25f9106562ea65ef273f936693e45b921b1
DeliaNav <mennavd@gmail.com>
2026-05-11 09:53:34 +0200

    arreglo de errores stripe

--- Codigo/views/carts/checkout_resultado.php
+++ Codigo/views/carts/checkout_resultado.php
@@ -1,4 +1,9 @@
-<?php require_once __DIR__ . '/../../views/layout/header.php'; ?>
+<?php 
+$items = $items ?? [];
+$total = $total ?? 0.00;
+
+require_once __DIR__ . '/../../views/layout/header.php';
+?>
 <link rel="stylesheet" href="/TFG/Codigo/public/css/checkout.css">
 
 <script src="https://js.stripe.com/v3/"></script>
@@ -9,27 +14,31 @@
 
         <div class="cart-layout">
             <div class="cart-items">
-                <?php foreach ($items as $item): ?>
-                    <div class="cart-item">
-                        <a href="/TFG/Codigo/cards/<?= htmlspecialchars($item['card_id']) ?>" class="cart-item-img">
-                            <img src="<?= htmlspecialchars($item['image_url']) ?>" alt="<?= htmlspecialchars($item['card_name']) ?>" onerror="this.src='/TFG/Codigo/public/img/card-placeholder.png'">
-                        </a>
-                        <div class="cart-item-info">
-                            <p class="cart-item-name"><?= htmlspecialchars($item['card_name']) ?></p>
-                            <p class="cart-item-seller">
-                                Vendido por <a href="/TFG/Codigo/usuario/<?= $item['seller_id'] ?>"><?= htmlspecialchars($item['seller_name']) ?></a>
-                            </p>
-                            <p class="cart-item-condition"><?= htmlspecialchars(ucfirst(str_replace('_', ' ', $item['condition']))) ?></p>
-                            <p class="cart-item-price"><?= number_format($item['price'], 2) ?> € / ud</p>
+                <?php if (empty($items)): ?>
+                    <p>No hay productos en el carrito.</p>
+                <?php else: ?>
+                    <?php foreach ($items as $item): ?>
+                        <div class="cart-item">
+                            <a href="/TFG/Codigo/cards/<?= htmlspecialchars($item['card_id']) ?>" class="cart-item-img">
+                                <img src="<?= htmlspecialchars($item['image_url']) ?>" alt="<?= htmlspecialchars($item['card_name']) ?>" onerror="this.src='/TFG/Codigo/public/img/card-placeholder.png'">
+                            </a>
+                            <div class="cart-item-info">
+                                <p class="cart-item-name"><?= htmlspecialchars($item['card_name']) ?></p>
+                                <p class="cart-item-seller">
+                                    Vendido por <a href="/TFG/Codigo/usuario/<?= $item['seller_id'] ?>"><?= htmlspecialchars($item['seller_name']) ?></a>
+                                </p>
+                                <p class="cart-item-condition"><?= htmlspecialchars(ucfirst(str_replace('_', ' ', $item['condition']))) ?></p>
+                                <p class="cart-item-price"><?= number_format($item['price'], 2) ?> € / ud</p>
+                            </div>
+                            <div class="cart-item-qty">
+                                <span class="qty-label">Cantidad: <strong><?= $item['quantity'] ?></strong></span>
+                            </div>
+                            <div class="cart-item-subtotal">
+                                <span><?= number_format($item['price'] * $item['quantity'], 2) ?> €</span>
+                            </div>
                         </div>
-                        <div class="cart-item-qty">
-                            <span class="qty-label">Cantidad: <strong><?= $item['quantity'] ?></strong></span>
-                        </div>
-                        <div class="cart-item-subtotal">
-                            <span><?= number_format($item['price'] * $item['quantity'], 2) ?> €</span>
-                        </div>
-                    </div>
-                <?php endforeach; ?>
+                    <?php endforeach; ?>
+                <?php endif; ?>
             </div>
 
             <div class="cart-summary">
@@ -48,7 +57,7 @@
 
                 <button id="btn-pagar" class="btn-checkout" disabled>
                     <span id="btn-texto">Pagar <?= number_format($total, 2) ?> €</span>
-                    <span id="btn-cargando">Procesando…</span>
+                    <span id="btn-cargando" style="display:none;">Procesando…</span>
                 </button>
 
                 <p class="summary-note">Pago cifrado con Stripe</p>
@@ -57,9 +66,8 @@
     </div>
 </div>
 
-<!-- Variables de PHP que necesita el JS -->
 <script>
-    const stripePublishableKey = '<?= STRIPE_PUBLISHABLE_KEY ?>';
+    const stripePublishableKey = '<?= defined('STRIPE_PUBLISHABLE_KEY') ? STRIPE_PUBLISHABLE_KEY : '' ?>';
     const stripeReturnUrl      = '<?= (isset($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] ?>/TFG/Codigo/carrito/resultado';
 </script>
 
